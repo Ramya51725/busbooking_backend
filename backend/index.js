700afff93c6967153433ad8a3782d70fa28afdb4
Ramya51725 <radhi240308@gmail.com>
2026-04-09 18:20:59 +0530

    add vercel.json

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,78 @@
 import express from 'express'
-import {MongoClient} from 'mongodb'
+import { MongoClient } from 'mongodb'
 import cors from 'cors'
 import * as dotenv from 'dotenv'
-import http, { createServer } from 'http'
+
 import busRouter from './routers/bus.router.js'
 import userRouter from './routers/userRouters.js'
 import bookingRouter from './routers/booking.router.js'
 import adminRouter from './routers/admin.routes.js'
-dotenv.config()
 
+dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-const PORT = process.env.PORT || 5000
-export const MONGO_DATABASE = process.env.MONGO_DATABASE 
+export const MONGO_DATABASE = process.env.MONGO_DATABASE
 const MONGO_URL = process.env.MONGO_URL
+
 export const client = new MongoClient(MONGO_URL)
+
 client.connect()
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("DB Error:", err))
 
-
-app.use("/api/buses" , busRouter)
-app.use("/api/users",userRouter)
-app.use("/api/bookings",bookingRouter)
+app.use("/api/buses", busRouter)
+app.use("/api/users", userRouter)
+app.use("/api/bookings", bookingRouter)
 app.use("/api/admin", adminRouter)
 
-
-
-app.get ("/", (req, res) => {
-    res.send ({
-        message : "Welcome to product express"
-})
+app.get("/", (req, res) => {
+  res.send({ message: "Welcome to product express" })
 })
 
-const server = http.createServer(app)
+export default app
 
-server.listen(PORT , () => {
-    console.log(`the app is running on port ${PORT}`)
-})
+
+
+// import express from 'express'
+// import {MongoClient} from 'mongodb'
+// import cors from 'cors'
+// import * as dotenv from 'dotenv'
+// import http, { createServer } from 'http'
+// import busRouter from './routers/bus.router.js'
+// import userRouter from './routers/userRouters.js'
+// import bookingRouter from './routers/booking.router.js'
+// import adminRouter from './routers/admin.routes.js'
+// dotenv.config()
+
+
+// const app = express()
+// app.use(cors())
+// app.use(express.json())
+
+// const PORT = process.env.PORT || 5000
+// export const MONGO_DATABASE = process.env.MONGO_DATABASE 
+// const MONGO_URL = process.env.MONGO_URL
+// export const client = new MongoClient(MONGO_URL)
+// client.connect()
+
+
+// app.use("/api/buses" , busRouter)
+// app.use("/api/users",userRouter)
+// app.use("/api/bookings",bookingRouter)
+// app.use("/api/admin",adminRouter)
+
+
+// app.get ("/", (req, res) => {
+//     res.send ({
+//         message : "Welcome to product express"
+// })
+// })
+
+// const server = http.createServer(app)
+
+// server.listen(PORT , () => {
+//     console.log(`the app is running on port ${PORT}`)
+// })
